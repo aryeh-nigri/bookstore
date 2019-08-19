552c58c7def26076e95cf07328d0a44085918a67
zeevNoiman <zevnoiman@gmail.com>
2019-08-19 21:36:09 +0300

    ok
    no

---
+++ bookstore/frontend/src/components/BooksAdministration.js
@@ -0,0 +1,9 @@
+import React, { Component } from 'react';
+
+// import { Container } from './styles';
+
+export default class BooksAdministration extends Component {
+  render() {
+    return <div />;
+  }
+}
