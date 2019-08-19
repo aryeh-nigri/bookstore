949fffc0a016d1557a68c9664d88dd8de64ee1da
Aryeh Nigri <aryehnigri@gmail.com>
2019-08-19 19:22:50 +0300

    modelo criado, route e server tb

--- bookstore/backend/server.js
+++ bookstore/backend/server.js
@@ -1,6 +1,4 @@
 const express = require("express");
-const server = require('http').Server(express);
-const io = require('socket.io')(server);
 const mongoose = require("mongoose");
 const cors = require('cors');
 
@@ -28,6 +26,8 @@ app.use(cors());
 app.use("/api/users", require("./routes/api/users"));
 app.use("/api/books", require("./routes/api/books"));
 app.use("/api/auth", require("./routes/api/auth"));
+// posts route
+app.use('/posts', require('./routes/posts'));
 
 // // Serve static assets if in production
 // if (process.env.NODE_ENV === 'production') {
@@ -41,114 +41,60 @@ app.use("/api/auth", require("./routes/api/auth"));
 
 const port = process.env.PORT || 8080;
 
-app.listen(port, () => console.log(`Server started on port ${port}`));
+// This is the listen before Socket.io
+// app.listen(port, () => console.log(`Server started on port ${port}`));
 
 // Socket.io
+//require the http module
+const http = require('http').Server(app)
 
-const socketPort = 80;
-server.listen(socketPort, () => console.log(`WebSocket listening to port ${socketPort}`));
+// require the socket.io module
+const io = require('socket.io');
 
-let pot = 0;
-let names = [];
-let serverNames = [];
-io.on('connection', socket => {
-  // below we listen if our pot is updated
-  // then emit an event to all connected sockets about the update
-  socket.on('UPDATE_POT', state => {
-    pot = state.pot;
-    socket.broadcast.emit('UPDATED_POT', state);
+const socket = io(http);
+//create an event listener
+
+//To listen to messages
+socket.on('connection', (socket) => {
+
+  console.log('User connected');
+
+  socket.on("disconnect", function () {
+    console.log("user disconnected");
   });
 
-  // get the current pot's value and emit it to clients
-  socket.on('GET_CURRENT_POT', () => socket.emit('CURRENT_POT', pot));
+  socket.on("newPost", function (post) {
 
-  // add the newest client to the list of active clients
-  // then broadcast that to all connected clienhts 
-  socket.on('SEND_NAME_TO_SERVER', name => {
-    serverNames = [...serverNames, { socketId: socket.id, name }];
-    names = [...names, name];
-    socket.broadcast.emit('SEND_NAMES_TO_CLIENTS', names);
-    socket.emit('SEND_NAMES_TO_CLIENTS', names);
+    console.log("Post: " + post);
+
+    // Broadcast message to everyone
+    socket.broadcast.emit("postReceived", { post });
+
+    const Post = require("../models/Post");
+
+    // Save post to the database
+    const newPost = new Post({
+      message: post.message,
+      author: post.author,
+      bookId: post.bookId
+    });
+
+    Post.addPost(newPost, (err, post) => {
+      if (err) throw err;
+      console.log('post added')
+    });
+
+    //   connect.then(db => {
+    //     console.log("connected correctly to the server");
+
+    //     let chatMessage = new Chat({ message: msg, sender: "Anonymous" });
+    //     chatMessage.save();
+    //   });
+    // });
+
   });
 
-  // broadcast to everyone if somebody pitched in
-  socket.on('SOMEONE_PITCHED_IN', name => {
-    socket.broadcast.emit('GUESS_WHO_PITCHED_IN', name);
+  //wire up the server to listen to our port
+  http.listen(port, () => {
+    console.log(`Connected to port ${port}`);
   });
-
-  // broadcast to everyone if somebody got one
-  socket.on('SOMEONE_GOT_ONE', name => {
-    socket.broadcast.emit('GUESS_WHO_GOT_ONE', name);
-  });
-
-
-  // this is to make sure that when a client disconnects
-  // the client's name will be removed from our server's list of names
-  // then broadcast that to everybody connected so their list will be updated
-  socket.on('disconnect', () => {
-    serverNames = serverNames.filter(data => data.socketId !== socket.id);
-    names = serverNames.map(data => data.name);
-    socket.broadcast.emit('SEND_NAMES_TO_CLIENTS', names);
-    socket.emit('SEND_NAMES_TO_CLIENTS', names);
-  });
-});
-
-
-// NOVO
-
-// // /server.js
-// const app = require('express')();
-// const server = require('http').Server(app);
-// const io = require('socket.io')(server);
-// const cors = require('cors');
-
-// // we will use port 8000 for our app
-// server.listen(8000, () => console.log('connected to port 8000!'));
-
-// // only use this for dev purposes
-// app.use(cors());
-
-// let pot = 0;
-// let names = [];
-// let serverNames = [];
-// io.on('connection', socket => {
-//   // below we listen if our pot is updated
-//   // then emit an event to all connected sockets about the update
-//   socket.on('UPDATE_POT', state => {
-//     pot = state.pot;
-//     socket.broadcast.emit('UPDATED_POT', state);
-//   });
-
-//   // get the current pot's value and emit it to clients
-//   socket.on('GET_CURRENT_POT', () => socket.emit('CURRENT_POT', pot));
-
-//   // add the newest client to the list of active clients
-//   // then broadcast that to all connected clienhts 
-//   socket.on('SEND_NAME_TO_SERVER', name => {
-//     serverNames = [...serverNames, { socketId: socket.id, name }];
-//     names = [...names, name];
-//     socket.broadcast.emit('SEND_NAMES_TO_CLIENTS', names);
-//     socket.emit('SEND_NAMES_TO_CLIENTS', names);
-//   });
-
-//   // broadcast to everyone if somebody pitched in
-//   socket.on('SOMEONE_PITCHED_IN', name => {
-//     socket.broadcast.emit('GUESS_WHO_PITCHED_IN', name);
-//   });
-
-//   // broadcast to everyone if somebody got one
-//   socket.on('SOMEONE_GOT_ONE', name => {
-//     socket.broadcast.emit('GUESS_WHO_GOT_ONE', name);
-//   });
-
-
-//   // this is to make sure that when a client disconnects
-//   // the client's name will be removed from our server's list of names
-//   // then broadcast that to everybody connected so their list will be updated
-//   socket.on('disconnect', () => {
-//     serverNames = serverNames.filter(data => data.socketId !== socket.id);
-//     names = serverNames.map(data => data.name);
-//     socket.broadcast.emit('SEND_NAMES_TO_CLIENTS', names);
-//     socket.emit('SEND_NAMES_TO_CLIENTS', names);
-//   });
-// });
